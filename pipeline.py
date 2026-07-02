782889db94103f4a68780c4be57ea42271c59cd5
Neilson-Ch <christopherneilson05@gmail.com>
2026-07-02 23:09:12 +0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -6,11 +6,11 @@ from evaluation import ModelEvaluator
 class PredictionPipeline:
     """The master pipeline orchestrating ingestion, training, evaluation, and approval."""
     
-    def __init__(self, raw_data_path: str | Path, accuracy_threshold: float = 0.7):
+    def __init__(self, raw_data_path: str | Path, f1_threshold: float = 0.7):
         self.base_dir = Path(__file__).parent
         self.raw_data_path = Path(raw_data_path)
         self.ingested_dir = self.base_dir / "ingested"
-        self.accuracy_threshold = accuracy_threshold
+        self.f1_threshold = f1_threshold
         
         # Core components instantiation
         self.ingestor = DataIngestion(self.raw_data_path, self.ingested_dir)
@@ -27,14 +27,14 @@ class PredictionPipeline:
         run_id, x_test, y_test = self.trainer.run(ingested_file_path)
         
         # 3. Handle telemetry evaluations and metrics verification
-        accuracy, precision, recall = self.evaluator.run(run_id, x_test, y_test)
+        accuracy, precision, recall, f1_weighted = (self.evaluator.run(run_id, x_test, y_test))
         
         # 4. Final conditional release assessment
         print("\n--- Deployment Approval Decision ---")
-        if accuracy >= self.accuracy_threshold:
+        if f1_weighted >= self.f1_threshold:
             print("🎉 Success: Model metrics pass QA. Approved for deployment!")
         else:
-            print(f"❌ Rejected: Model accuracy ({accuracy:.3f}) falls short of threshold ({self.accuracy_threshold})")
+            print(f"❌ Rejected: Model f1 weighted ({f1_weighted:.3f}) falls short of threshold ({self.f1_threshold})")
 
 
 if __name__ == "__main__":
@@ -42,5 +42,5 @@ if __name__ == "__main__":
     DATA_INPUT = Path(__file__).parent / "data_A.csv"
     
     # Initialize and execute
-    pipeline = PredictionPipeline(raw_data_path=DATA_INPUT, accuracy_threshold=0.7)
+    pipeline = PredictionPipeline(raw_data_path=DATA_INPUT, f1_threshold=0.7)
     pipeline.execute()
